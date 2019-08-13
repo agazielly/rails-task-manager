0907c340722b3c719ba8874a14085acc5d36e865
Alan Gazielly <alan@gazielly.com>
2019-08-13 17:07:14 +0200

    Exercise 2 without CSS.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Read all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Read one task
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # Create a new task
+  post 'tasks', to: 'tasks#create'
+
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
